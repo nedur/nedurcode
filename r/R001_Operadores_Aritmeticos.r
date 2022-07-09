832b56c4047b9b48854c429c5d269f49988dd99f
Vinicius Vale <63558209+viniciusvale@users.noreply.github.com>
2022-07-09 12:28:08 -0300

    Add files via upload

---
+++ r/R001_Operadores_Aritmeticos.r
@@ -0,0 +1,14 @@
+# <NEDUR/code>
+# </> R001 - Operadores Aritméticos
+
+# Operações básicas
+
+2 + 2   # Adição
+4 - 2   # Subtração
+3 * 2   # Multiplicação
+4 / 2   # Divisão
+4 ^ 2   # Exponencial
+4 ** 2  # Exponencial
+5 %/% 2 # Divisão inteira
+
+
